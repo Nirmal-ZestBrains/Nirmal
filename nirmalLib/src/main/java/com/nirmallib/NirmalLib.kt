6a7a63445bead085e413bf0399e3e75452999499
Nim's <vaghasiyanirmal@gmail.com>
2021-01-02 21:38:39 +0530

    Initial commit

---
+++ nirmalLib/src/main/java/com/nirmallib/NirmalLib.kt
@@ -0,0 +1,12 @@
+package com.nirmallib
+
+import android.content.Context
+import android.widget.Toast
+
+object NirmalLib {
+
+    fun lOG(context: Context){
+        Toast.makeText(context,"Hello nirmal",Toast.LENGTH_SHORT).show()
+    }
+
+}
